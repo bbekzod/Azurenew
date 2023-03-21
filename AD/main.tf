7c21b33eeaa7057d53a1567fd15058757aa5da25
EC2 Default User <ec2-user@ip-172-31-29-150.eu-west-1.compute.internal>
2023-03-21 21:12:53 +0000

    Add main.tf

---
+++ AD/main.tf
@@ -0,0 +1,6 @@
+resource "azuread_user" "example" {
+  user_principal_name = "jdoe@hashicorp.com"
+  display_name        = "J. Doe"
+  mail_nickname       = "jdoe"
+  password            = "SecretP@sswd99!"
+}
